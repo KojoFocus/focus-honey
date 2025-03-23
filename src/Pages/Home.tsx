8bf58a890a1750e681d3c5ea4e6d32d7580adcf5
KojoFocus <ernestobimpeh9@gmail.com>
2025-03-23 09:42:50 +0000

    took the min-h away to shorten the height of the home

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"; // Import Link for navigation
 
 const HomePage = () => {
   return (
-    <div className="bg-[#636363] text-center min-h-screen">
+    <div className="bg-[#636363] text-center ">
       {" "}
       {/* Full-screen background */}
       {/* Header */}
